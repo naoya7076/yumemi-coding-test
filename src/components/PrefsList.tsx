5ee6e0717125eeff1cfbc56dd8c09d41c3d29659
naoya7076 <naoya7076@gmail.com>
2024-03-03 14:44:19 +0900

    ロード中のガードを手前に作成したので、return内でのガード節を削除

--- src/components/PrefsList.tsx
+++ src/components/PrefsList.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useGetPrefs } from "../hooks/useGetPrefs";
 
 export const PrefsList: React.FC = () => {
-    const { result, error } = useGetPrefs();
+    const { result, error, isLoading } = useGetPrefs();
     if (error) return <div>Error loading preferences</div>;
+    if (isLoading) return <div>Loading preferences</div>;
     return (
         <div>
-            {result && result.map((pref, index) => (
+            {result.map((pref, index) => (
                 <label key={index}>
                     <input type="checkbox" value={pref.prefCode} />
                     {pref.prefName}
