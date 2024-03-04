6599de46e8e658953bf9f59617494298da99f124
naoya7076 <naoya7076@gmail.com>
2024-03-04 15:34:51 +0900

    固定値で複数のグラフを取得してチャートを表示させる

--- src/components/PrefsPopulationChart.tsx
+++ src/components/PrefsPopulationChart.tsx
@@ -1,34 +1,47 @@
 import React from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
 import {useGetPrefsPopulation}  from "../hooks/useGetPrefsPopulation";
 
-export const PrefsPopulationChart: React.FC<{prefCode: number}> = ({prefCode}) => {
-    const { result, error, isLoading } = useGetPrefsPopulation(prefCode);
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export const PrefsPopulationChart: React.FC<{prefCodes: number[]}> = ({prefCodes}) => {
+    const { results, error, isLoading } = useGetPrefsPopulation(prefCodes);
     if (error) return <div>Error loading preferences</div>;
     if (isLoading) return <div>Loading preferences</div>;
-    const totalPopulationList = result.data[0].data; // Retrieve data using label name instead of array index
+    const labels = results[0].result.data[0].data.map((d) => d.year);
+    const graphData = results.map(({result},index) => {
+        return {
+            label: prefCodes[index].toString(),
+            data: result.data[0].data.map((d) => d.value),
+        };
+    });
+    const data = {
+        labels,
+        datasets: graphData
+    };
+    // TODO: 色が真っ黒なので後で直す
     return (
-        <ResponsiveContainer width="100%" height="100%" minHeight={300} minWidth={100}>
-            <LineChart
-                data={totalPopulationList}
-                width={500}
-                height={300}
-                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-            >
-                <XAxis dataKey="year" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Legend />
-                <Line
-                    type="monotone"
-                    dataKey="value"
-                    stroke="#8884d8"
-                    activeDot={{ r: 8 }}
-                />
-            </LineChart>
-        </ResponsiveContainer>
+        <div>
+            <Line data={data} />
+        </div>
     );
-};
+}
 
--- src/hooks/useGetPrefsPopulation.ts
+++ src/hooks/useGetPrefsPopulation.ts
@@ -1,22 +1,31 @@
 import useSWR from "swr";
 import { fetcher } from "./fetcher";
 type PopulationType = {
-  boundaryYear: number;
   data: {
-    label: string;
-    data: {
-      year: number;
-      value: number;
-    }[];
+    year: number;
+    value: number;
   }[];
 };
-export const useGetPrefsPopulation = (prefCode: number) => {
-  const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`;
-  const { data, error, isLoading, isValidating } = useSWR(url, (url: string) =>
-    fetcher(url, {
-      "X-API-KEY": `${import.meta.env.VITE_RESAS_API_KEY}`,
-    })
+type ResponseType = {
+  message: string | null;
+  result: {
+    boundaryYear: number;
+    data: PopulationType[];
+  };
+};
+export const useGetPrefsPopulation = (prefCodes: number[]) => {
+  const { data, error, isLoading, isValidating } = useSWR(
+    prefCodes,
+    (prefCodes: number[]) =>
+      Promise.all(
+        prefCodes.map((prefCode) => {
+          const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`;
+          return fetcher(url, {
+            "X-API-KEY": `${import.meta.env.VITE_RESAS_API_KEY}`,
+          });
+        })
+      )
   );
-  const result: PopulationType = data?.result;
-  return { result, error, isLoading, isValidating };
+  const results = data as ResponseType[];
+  return { results, error, isLoading, isValidating };
 };
