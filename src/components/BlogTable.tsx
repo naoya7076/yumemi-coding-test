69a1e48c76696b1c953a936f65dbb23f20d36ade
naoya7076 <naoya7076@gmail.com>
2024-03-17 22:08:48 +0900

    表示記事数の処理を追加

--- src/components/BlogTable.tsx
+++ src/components/BlogTable.tsx
@@ -1,15 +1,24 @@
 import { useState, useMemo } from "react"
 import { useArticle } from "../hooks/useArticle"
 export const BlogTable:React.FC = () => {
-    const ITEMS_PER_PAGE = 5
+    const [itemsPP, setItemsPP] = useState<number>(5)
     const {articles} = useArticle()
     const [page, setPage] = useState(1)
     const displayArticles = useMemo(() =>{
-        const start = (page -1) * ITEMS_PER_PAGE
-        return articles.slice(start, start + ITEMS_PER_PAGE)
-    },[articles, page]);
+        const start = (page -1) * itemsPP
+        return articles.slice(start, start + itemsPP)
+    },[articles, page, itemsPP]);
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setItemsPP(parseInt(e.target.value))
+        setPage(1)
+    }
     return(
         <>
+            <select onChange={handleSelect}>
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={20}>20</option>
+            </select>
             <table>
                 <thead>
                     {displayArticles.map((article,index) =>
